bef12fea832bc4ca20dd534d7b9e194413562c91
Bernardo Bertante <bernardo.sctp@gmail.com>
2025-11-24 21:27:20 -0300

    adcionado o save de usuarios

--- src/main/java/com/agendatarefa/usuario/dto/UsuarioDTO.java
+++ src/main/java/com/agendatarefa/usuario/dto/UsuarioDTO.java
@@ -1,14 +1,11 @@
 package com.agendatarefa.usuario.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
-@Getter
-@Setter
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UsuarioDTO {
